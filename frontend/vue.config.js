7b11575bef2627532407063072f258d2677a6ed3
Aziz Ashurov <a.o.ashurov@gmail.com>
2021-08-09 17:51:50 +0500

    first commit

--- frontend/vue.config.js
+++ frontend/vue.config.js
@@ -1,10 +1,8 @@
 const BundleTracker = require("webpack-bundle-tracker");
 
 module.exports = {
-
     publicPath: "http://127.0.0.1:8000/",
     outputDir: './dist/',
-
     chainWebpack: config => {
 
         config.optimization
