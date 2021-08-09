9c983e6c7091a4768948c67ba4cbd148658abe91
Aziz Ashurov <a.o.ashurov@gmail.com>
2021-08-09 22:02:18 +0500

    first commit

--- EDSystem/settings.py
+++ EDSystem/settings.py
@@ -190,6 +190,13 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "assets"),
+    os.path.join(BASE_DIR, "frontend/dist"),
+]
+
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
